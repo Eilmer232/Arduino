0954116bd1b276741f53f3621dcb275bbc1cb9f3
Eilmer <toroeilmer@gmail.com>
2024-06-27 15:21:52 -0500

    Updated website design to match Duolingo style

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        alert('Message sent!');
+        alert('Mensaje enviado!');
     });
 });
