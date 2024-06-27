55b0597092f933e8cbfa234bf5e86ffb997270cb
Eilmer <toroeilmer@gmail.com>
2024-06-27 13:32:40 -0500

    Descripción de los cambios realizados

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page loaded');
+    
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert('Message sent!');
+    });
 });
